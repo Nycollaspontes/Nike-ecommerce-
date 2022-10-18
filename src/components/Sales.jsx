0372c58beaeadffa00003784bed569ba9cdf92c6
Nycollas Pontes <nycollaspontes@gmail.com>
2022-10-18 00:48:29 -0300

    Card Add

---
+++ src/components/Sales.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "./utils/Card";
+import Title from "./utils/Title";
+
+const Sales = ({ endpoint: { title, items, ifExists } }) => {
+  return (
+    <>
+      <div>
+        <Title title={title} />
+        <div
+          className={`grid items-center justify-items-center ${
+            ifExists
+              ? "grid-cols-3 xl:grid-cols-2 sm:grid-cols-1"
+              : "grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-7 lg:gap-5 mt-7"
+          } 
+                     `}
+        >
+          {items.map((item, i) => (
+            <Card {...item} key={i} ifExists={ifExists} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sales;
--- src/components/utils/SocialLink.jsx
+++ src/components/utils/SocialLink.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const SocialLink = ({icon}) => {
+const SocialLink = ({ icon }) => {
   return (
-    <img src={icon} alt="Social/Icon"
-    className='w-8 h-8 flex items-center cursor-pointer md:-6 md:h-6 sm:w-5 sm:h-5 transition-all duration-200 hover:scale-110 '/>
-  )
-}
+    <img
+      src={icon}
+      alt="Social/Icon"
+      className="w-8 h-8 flex items-center cursor-pointer md:-6 md:h-6 sm:w-5 sm:h-5 transition-all duration-200 hover:scale-110 "
+    />
+  );
+};
 
-export default SocialLink
+export default SocialLink;
