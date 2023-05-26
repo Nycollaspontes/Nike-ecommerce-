2dab4de9c6bad685331d60d40d16e6b7f5f756d0
nycollas pontes <nycollaspontes@gmail.com>
2023-05-26 13:44:31 -0300

    finalized

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -12,7 +12,7 @@ const Sales = ({ ifExists , endpoint: { title, items } }) => {
         <div
           className={`grid items-center justify-items-center ${
             ifExists
-              ? "grid-cols-3 xl:grid-cols-2 sm:grid-cols-1 lg:gap-12"
+              ? "grid-cols-3 gap-6 xl:grid-cols-2 sm:grid-cols-1 lg:gap-12"
               : "grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-7 lg:gap-5 mt-7"
           } 
                      `}
